88c3a7bd2ddaaa8271003d63c0a41c50ed1e0512
shmily0827 <shmily@email.cufe.edu.cn>
2016-11-15 23:25:34 +0800

    Update c6.sh

--- c6.sh
+++ c6.sh
@@ -1,3 +1,3 @@
 rpm -ivh http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
 rpm -ivh http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
-reboot
+echo “reboot please”
